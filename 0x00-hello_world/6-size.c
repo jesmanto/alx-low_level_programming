60353c9e2816649269f47ec144877029ae59e6e5
Jesse Okoro <okorojesse@gmail.com>
2023-07-14 11:14:01 +0000

    print the sizes of all data types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,10 +9,11 @@ int main(void)
 {
 	int charsize = sizeof(char);
 	int intsize = sizeof(int);
-	int longsize = sizeof(long);
+	int longsize = sizeof(long int);
+	int llongsize = sizeof(long int);
 	int floatsize = sizeof(float);
 
-	printf("Size of char: %i byte(s)\nSize of an int: %i byte(s)\nSize of a long int: %i byte(s)\nSize of a long long int: %i byte(s)\nSize of a float: %i byte(s)\n", charsize, intsize, longsize, longsize, floatsize);
+	printf("Size of char: %i byte(s)\nSize of an int: %i byte(s)\nSize of a long int: %i byte(s)\nSize of a long long int: %i byte(s)\nSize of a float: %i byte(s)\n", charsize, intsize, longsize, llongsize, floatsize);
 
 	return (0);
 }
