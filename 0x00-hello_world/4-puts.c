25653b249deb9bd1d5921bdab4f1ad4bcc1ceafd
Jesse Okoro <okorojesse@gmail.com>
2023-07-14 08:54:51 +0000

    print using puts in c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char Message[] = "Programming is like building a multilingual puzzle";
+
+	puts(Message);
+	return (0);
+}
