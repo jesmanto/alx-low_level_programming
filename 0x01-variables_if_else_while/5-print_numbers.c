57a85dd6577700cdfcceb3113c76555e75660540
Jesse Okoro <okorojesse@gmail.com>
2023-07-15 14:27:44 +0000

    print all single base 10 numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%i", i);
+
+	printf("\n");
+	return (0);
+}
