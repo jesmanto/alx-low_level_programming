9d22b568de60ab30ddb6a183f2e3c5140575b244
Jesse Okoro <okorojesse@gmail.com>
2023-07-15 14:46:49 +0000

    print lowercase alphabets in reverse mode

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 122; n > 96; n--)
+		putchar(n);
+	putchar(10);
+
+	return (0);
+}
