dde26c259b6ce6fc2c2bd51f0d8e61a1db6c6f38
Jesse Okoro <okorojesse@gmail.com>
2023-07-19 11:21:47 +0000

    print signs only

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <std.io>
+
+/**
+ * print_sign - prints only signs
+ *
+ * @n: integer param
+ *
+ * Return: 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
+
