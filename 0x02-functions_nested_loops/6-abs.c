f2a2c00d99e0be41e9ee9ec3a966ee74223e6dc3
Jesse Okoro <okorojesse@gmail.com>
2023-07-19 14:06:32 +0000

    print absolute number of the input

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _abs - absolute value
+ *
+ * @
+ *
+ * Return: 0
+ */
+int _abs(int n)
+{
+	int absval = n;
+
+	if (n < 0)
+	{
+		absval = -1 * n;
+	}
+
+	return (absval);
+}
