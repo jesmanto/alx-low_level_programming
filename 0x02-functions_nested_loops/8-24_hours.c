131ec1d8952e39b539922eca16d7580f9eabbfee
Jesse Okoro <okorojesse@gmail.com>
2023-07-19 15:22:27 +0000

    display every minute

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * jack_bauer - print hours and minutes
+ *
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int h, r, m, n;
+
+	h = 0;
+
+	while (h <= 2)
+	{
+		_putchar(h);
+		r = 0;
+
+		while (r <= 4)
+		{
+			_putchar(r);
+			m = 0;
+
+			while (m <= 5)
+			{
+				n = 0;
+
+				while (n <= 9)
+				{
+					_putchar(h);
+					_putchar(r);
+					_putchar(':');
+					_putchar(m);
+					_putchar(n);
+					_putchar('\n');
+
+					n++;
+				}
+				m++;
+			}
+			r++;
+		}
+		h++;
+	}
+}
---
+++ 0x02-functions_nested_loops/8-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	jack_bauer();
+	return (0);
+}
