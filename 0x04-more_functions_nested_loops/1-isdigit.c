6e4816a8007ca5278b57069e98809721c5e75c60
Jesse Okoro <okorojesse@gmail.com>
2023-07-20 11:46:51 +0000

    check for digits

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isdigit - checks if input is a digit
+ *
+ * @c: input param
+ *
+ * Return: 0
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
+}
