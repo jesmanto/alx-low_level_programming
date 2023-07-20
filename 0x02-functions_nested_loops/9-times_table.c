13aaf5f342baae989427537ee5ee905f97e10796
Jesse Okoro <okorojesse@gmail.com>
2023-07-20 04:33:26 +0000

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,16 +9,28 @@ void times_table(void)
 {
 	int r, c, product;
 
-	for (r = '0'; r <= '9'; r++)
+	for (r = 0; r <= 9; r++)
 	{
-		for (c = '0'; c <= '9'; c++)
+		for (c = 0; c <= '9'; c++)
 		{
 			product = r * c;
 
-			_putchar(product);
-			if (c < '9')
+			int m = product % 10;
+
+			int d = product / 10;
+
+			if (product <= 9)
+				_putchar(product)
+			else
 			{
-				if (product > '9')
+				_putchar(d);
+				_putchar(m);
+			}
+
+			_putchar(product);
+			if (c < 9)
+			{
+				if (product > 9)
 				{
 					_putchar(',');
 					_putchar(' ');
