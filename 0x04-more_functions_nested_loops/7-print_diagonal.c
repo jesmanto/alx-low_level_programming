5d3b8fbec40e11cada8ce3dc3304f06b08255cd4
Jesse Okoro <okorojesse@gmail.com>
2023-07-20 15:03:12 +0000

    print in diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagonal - print in diagonal format
+ *
+ * @n: diagonal length
+ *
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int l, b;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		l = 1;
+
+		while (l <= n)
+		{
+			b = 1;
+
+			while (b <= n)
+			{
+				if (b != l)
+					_putchar(' ');
+				else
+					_putchar('\\');
+				b++;
+			}
+			l++;
+			putchar('\n');
+		}
+	}
+}
