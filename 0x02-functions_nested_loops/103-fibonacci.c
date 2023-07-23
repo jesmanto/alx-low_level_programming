81300e53293ecb3a174111fff66a760994c02e6c
Jesse Okoro <okorojesse@gmail.com>
2023-07-23 11:02:15 +0000

    even liber abbaci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -6,20 +6,22 @@
  */
 int main(void)
 {
-	long int i, sum, n1, n2;
+	long int n, i, sum, n1, n2;
 
 	n1 = 0;
 	n2 = 1;
 	sum = 0;
 	i = 0;
+	n = 0;
 
 	while (i < 4000000)
 	{
+		n1 = n;
 
 		if ((n2 + n1) % 2 == 0)
 			sum += (n2 + n1);
 
-		n1 = n2;
+		n = n2;
 		n2 += n1;
 		i = n2;
 	}
