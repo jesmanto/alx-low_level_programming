de98b03efdd7a3c59f8e6b556338821f15e21374
Jesse Okoro <okorojesse@gmail.com>
2023-07-25 13:26:32 +0000

    print characters

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * puts2 - print characters
+ * @str: array of characters
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int length, count;
+
+	length = strlen(str);
+	count = 0;
+
+	while (count < length)
+	{
+		if (str[count] % 2 == 0)
+			putchar(str[count]);
+		count++;
+	}
+	putchar('\n');
+}
