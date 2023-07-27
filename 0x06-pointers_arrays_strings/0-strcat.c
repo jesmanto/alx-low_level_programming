5a8fe1bb2a3e73e36af4aa34321aa064ba135229
Jesse Okoro <okorojesse@gmail.com>
2023-07-27 01:01:17 +0000

    concatenate strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,5 +10,17 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	return (strcat(dest, src));
+	int size, i;
+
+	size = 0;
+
+	while (dest[size] != '\0')
+		size++;
+
+	for (i = 0; i < size && src[i] != '\0'; i++)
+		dest[size + i] = src[i];
+
+	dest[size + i] = '\0';
+
+	return (dest);
 }
