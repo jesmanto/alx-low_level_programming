02cfe32bdcadfbda73167012054a8d16eaded7c5
Jesse Okoro <okorojesse@gmail.com>
2023-07-28 13:19:20 +0000

    capitalize

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,13 +7,16 @@
  */
 char *cap_string(char *c)
 {
-	int separator, i;
+	int *pts;
+	char *ptr;
 
-	separator = 1;
-	i = 0;
+	int separator = 1;
+	int i = 0;
 
 	while (c[i] != '\0')
 	{
+		pts = &separator;
+		ptr = &c[i];
 		if (separator == 1)
 		{
 			if (c[i] >= 'a' && c[i] <= 'z')
@@ -21,59 +24,39 @@ char *cap_string(char *c)
 				c[i] -= 32;
 				separator = 0;
 			}
+			else if (c[i] == '.' || c[i] == '!' || c[i] == '?' || c[i] == ',')
+				iterator(pts, 1, ptr, c[i]);
+			else if (c[i] == 34 || c[i] == 40 || c[i] == 41 || c[i] == 59 || c[i] == 9)
+				iterator(pts, 1, ptr, c[i]);
+			else if (c[i] == '{' || c[i] == '}' || c[i] == 32 || c[i] == '\n')
+				iterator(pts, 1, ptr, c[i]);
 			else
-			{
-				if (c[i] == '\n' || c[i] == ',' || c[i] == ';')
-				{
-					c[i] = c[i];
-					separator = 1;
-				}
-				else if (c[i] == '.' || c[i] == '!' || c[i] == '?')
-				{
-					c[i] = c[i];
-					separator = 1;
-				}
-				else if (c[i] == 34 || c[i] == '(' || c[i] == ')')
-				{
-					c[i] = c[i];
-					separator = 1;
-				}
-				else if (c[i] == '{' || c[i] == '}' || c[i] == 32 || c[i] == 9)
-				{
-					c[i] = c[i];
-					separator = 1;
-				}
-				else
-				{
-					c[i] = c[i];
-					separator = 0;
-				}
-			}
+				iterator(pts, 0, ptr, c[i]);
 		}
 		else
 		{
-			if (c[i] == '\n' || c[i] == ',' || c[i] == ';')
-			{
-				c[i] = c[i];
-				separator = 1;
-			}
-			if (c[i] == '.' || c[i] == '!' || c[i] == '?')
-			{
-				c[i] = c[i];
-				separator = 1;
-			}
-			if (c[i] == 34 || c[i] == '(' || c[i] == ')')
-			{
-				c[i] = c[i];
-				separator = 1;
-			}
-			if (c[i] == '{' || c[i] == '}' || c[i] == 32 || c[i] == 9)
-			{
-				c[i] = c[i];
-				separator = 1;
-			}
+			if (c[i] == '.' || c[i] == '!' || c[i] == '?' || c[i] == '\n')
+				iterator(pts, 1, ptr, c[i]);
+			if (c[i] == 34 || c[i] == '(' || c[i] == ')' || c[i] == ',')
+				iterator(pts, 1, ptr, c[i]);
+			if (c[i] == '{' || c[i] == '}' || c[i] == 32 || c[i] == 9 || c[i] == ';')
+				iterator(pts, 1, ptr, c[i]);
 		}
 		i++;
 	}
 	return (c);
+}
+
+/**
+ * iterator - iterates the loop
+ * @separator: indicates if theers's a symbol
+ * @val: new value for separator
+ * @el: current element
+ * @e: new character after symbol
+ * Return: void
+ */
+void iterator(int *separator, int val, char *el, char e)
+{
+	*el = e;
+	*separator = val;
 }
