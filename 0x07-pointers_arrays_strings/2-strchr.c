b8da7d105216863403509d64046c3cab855781d6
Jesse Okoro <okorojesse@gmail.com>
2023-08-01 09:19:38 +0000

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * _strchr - locatesa character in a string
@@ -8,14 +10,15 @@
  */
 char *_strchr(char *s, char c)
 {
-	if (*s == c)
-		return (s);
-	
 	while (*s)
 	{
 		if (*s == c)
 			return (s);
+		s++;
 	}
+
+	if (*s == c)
+		return (s);
 
 	return (NULL);
 }
