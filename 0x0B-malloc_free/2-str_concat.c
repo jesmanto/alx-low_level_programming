49343f8271b3f01b62651795853cf33cb1941aa6
Jesse Okoro <okorojesse@gmail.com>
2023-08-09 04:49:43 +0000

    strconcat malloc

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+
+/**
+ * str_concat - allocatesmemory to joint strings
+ * @s1: First string
+ * @s2: Second string
+ * Return: pointer to the new string
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int l1, l2, i, n;
+	char *c;
+
+	l1 = 0;
+	l2 = 0;
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	while (*s1)
+	{
+		l1++;
+		s1++;
+	}
+
+	for (; s1[l1]; l1++)
+	{
+	}
+
+	for (; s2[l2]; l2++)
+	{
+	}
+
+	c = malloc(l1 + l2 + 1);
+
+	i = 0;
+	for (; s1[i]; i++)
+		c[i] = s1[i];
+	for (n = 0; i < l1 + l2; i++, n++)
+		c[i] = s2[n];
+
+	c[i] = '\0';
+	return (c);
+}
