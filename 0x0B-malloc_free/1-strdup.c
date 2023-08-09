fb55aae4d05fe02a59693ec9864a71e6d965a6fe
Jesse Okoro <okorojesse@gmail.com>
2023-08-09 04:19:37 +0000

    allocate memory to string copy

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+
+/**
+ * _strdup - allocate memory for a copied string
+ * @str: copied string
+ * Return: pointer to copy of string
+ */
+char *_strdup(char *str)
+{
+	int n;
+	char *c;
+
+	if (str == NULL)
+		return (NULL);
+	n = 0;
+	for (; str[n]; n++)
+	{
+	}
+
+	c = malloc(n + 1);
+	for (; n >= 0; n--)
+	{
+		c[n] = str[n];
+	}
+	return (c);
+}
