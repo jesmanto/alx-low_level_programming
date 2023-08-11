e2c51a5d9b36b3e3de0c08f622c7eefecc80834c
Jesse Okoro <okorojesse@gmail.com>
2023-08-11 04:32:17 +0000

    array range

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -14,7 +14,7 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-	pt = malloc(sizeof(int) * (max - min) * sizeof(int));
+	pt = malloc(sizeof(int) * (max - min) + sizeof(int));
 
 	if (pt == NULL)
 		return (NULL);
