40e447099d93273d2265c64606b83ef975875cc8
Jesse Okoro <okorojesse@gmail.com>
2023-08-16 08:46:33 +0000

    dog data type

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dog data type
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
