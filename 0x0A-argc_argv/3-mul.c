82a506b89db0ac6224146a9037c77dd2dc92be5b
Jesse Okoro <okorojesse@gmail.com>
2023-08-27 22:43:49 +0000

    multiply the arguments

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+/**
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0
+ */
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+	int i = 1;
+	int result = 1;
+
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	for (; i < argc; i++)
+		result *= atoi(argv[i]);
+	printf("%d\n", result);
+	exit(EXIT_SUCCESS);
+}
