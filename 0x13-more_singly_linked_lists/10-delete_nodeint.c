9ad9e39c0def992d38be51233514ce043c2f72f7
Jesse Okoro <okorojesse@gmail.com>
2023-08-30 13:59:58 +0000

    reverse the list

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -9,27 +9,29 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *initial = *head, *rem = NULL, *temp;
-	unsigned int i = 0;
+	listint_t *initial = *head, *rem = NULL;
+	unsigned int i = 1;
 
 	if (head == NULL)
 		return (-1);
+	for (; i < index; i++)
+	{
+		if (initial->next != NULL)
+			initial = initial->next;
+		else
+			return (-1);
+	}
 	if (index == 0)
 	{
-		temp = *head;
-		*head = (*head)->next;
-		free(temp);
-		return (1);
-	}
-	while (initial != NULL && i < index)
-	{
 		rem = initial;
-		initial = initial->next;
-		i++;
-	}
-	if (initial == NULL)
+		*head = (*head)->next;
 		return (-1);
-	rem->next = initial->next;
-	free(initial);
+	}
+	else
+	{
+		rem = initial->next;
+		initial->next = rem->next;
+	}
+	free(rem);
 	return (1);
 }
---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * reverse_lisitint - reverse a linked list
+ * @head: first node of linked list
+ * Return: pointer to the first node of the reversed list
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *prev = NULL, *curr = *head, *next;
+
+	while (curr != NULL)
+	{
+		next = curr->next;
+		curr->next = prev;
+		prev = curr;
+		curr = next;
+	}
+	*head = prev;
+	return (*head);
+}
